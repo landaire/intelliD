f0e551ad6d9eb2eaf842123b97bb6219202625fc
Lander Brandt <landerbrandt@gmail.com>
2014-10-21 11:39:36 -0700

    Code cleanup

--- dlang/src/main/java/com/wyrdtech/d/intellid/DFileTypeFactory.java
+++ dlang/src/main/java/com/wyrdtech/d/intellid/DFileTypeFactory.java
@@ -14,25 +14,4 @@ public class DFileTypeFactory extends FileTypeFactory {
     public void createFileTypes(@NotNull final FileTypeConsumer fileTypeConsumer) {
         fileTypeConsumer.consume(DFileType.INSTANCE, new ExtensionFileNameMatcher(DFileType.DEFAULT_EXTENSION));
     }
-/*
-
-    // Check if file is of D src type
-    public boolean isMyFileType(VirtualFile file) {
-        if (file == null) {
-            return false;
-        }
-        if (file.isDirectory()) {
-            return false;
-        }
-
-        if (DSrcFileType.DEFAULT_EXTENSION.equals(file.getExtension())) {
-            return true;
-        }
-
-        // TODO: implement contents inspection?
-
-        return false;
-    }
-*/
-
 }
